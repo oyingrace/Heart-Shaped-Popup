b249c176cef7650a6a04749758f2d2c20d2514b5
DevQueen <124140753+oyingrace@users.noreply.github.com>
2023-10-14 19:14:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let heart = document.getElementById("heart");
+
+function openHeart(){
+   heart.classList.add("open-heart");
+}
+function closeHeart(){
+    heart.classList.remove("open-heart");
+ }
